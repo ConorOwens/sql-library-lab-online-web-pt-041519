e0539ca25bc9c9c527634dad500250d18edd109a
Conor Owens <conorj.owens@gmail.com>
2019-06-26 03:01:33 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,32 +1,38 @@
 CREATE TABLE series (
 id INTEGER PRIMARY KEY,
 title TEXT,
-author INTEGER,
-sub-genre INTEGER
+author_id INTEGER,
+subgenre_id INTEGER
 );
 
-CREATE TABLE sub-genre (
+CREATE TABLE subgenres (
 id INTEGER PRIMARY KEY,
 name TEXT
 );
 
-CREATE TABLE author (
+CREATE TABLE authors (
 id INTEGER PRIMARY KEY,
 name TEXT
 );
 
-CREATE TABLE book (
+CREATE TABLE books (
 id INTEGER PRIMARY KEY,
 title TEXT,
 year INTEGER,
-series INTEGER
+series_id INTEGER
 );
 
-CREATE TABLE character (
+CREATE TABLE characters (
 id INTEGER PRIMARY KEY,
 name TEXT,
 motto TEXT,
 species TEXT,
-author INTEGER,
-series INTEGER
+author_id INTEGER,
+series_id INTEGER
+);
+
+CREATE TABLE character_books (
+id INTEGER PRIMARY KEY,
+book_id INTEGER,
+character_id INTEGER,
 );
