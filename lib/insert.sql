1be8f8c8b360cca87a40dec70cab3568b4853acb
Conor Owens <conorj.owens@gmail.com>
2019-06-26 03:12:18 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,6 +2,6 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES ("Cirque du Freak", "1
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Discworld", "2", "2");
 INSERT INTO subgenres (name) VALUES ("Horror");
 INSERT INTO subgenres (name) VALUES ("Fantasy");
-INSERT INTO authors (name) VALUES ("Gaiman");
+INSERT INTO authors (name) VALUES ("Darren Shan");
 INSERT INTO authors (name) VALUES ("Pratchett");
-INSERT INTO book (title, year, series_id) VALUES ("")
+INSERT INTO book (title, year, series_id) VALUES ("The Vampires Assistant", "1994", 1)
