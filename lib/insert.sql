0a5cbb7f633f742a80d7e3e06a3021041b27dcd1
Conor Owens <conorj.owens@gmail.com>
2019-06-26 03:09:24 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Bongos", "1", "1");
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Horns", "2", "2");
-INSERT INTO subgenres (name) VALUES ("Percussion");
-INSERT INTO subgenres (name) VALUES ("Wind");
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Discworld", "2", "2");
+INSERT INTO subgenres (name) VALUES ("");
+INSERT INTO subgenres (name) VALUES ("Fantasy");
 INSERT INTO authors (name) VALUES ("Gaiman");
 INSERT INTO authors (name) VALUES ("Pratchett");
-INSERT INTO book ()
+INSERT INTO book (title, year, series_id) VALUES ("")
