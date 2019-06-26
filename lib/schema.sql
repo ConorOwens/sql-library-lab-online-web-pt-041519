ed872b783ee374bef348189c034c762a79eba044
Conor Owens <conorj.owens@gmail.com>
2019-06-26 02:57:45 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -7,5 +7,26 @@ sub-genre INTEGER
 
 CREATE TABLE sub-genre (
 id PRIMARY KEY INTEGER,
+name TEXT
+);
 
+CREATE TABLE author (
+id PRIMARY KEY INTEGER,
+name TEXT
+);
+
+CREATE TABLE book (
+id PRIMARY KEY INTEGER,
+title TEXT,
+year INTEGER,
+series INTEGER
+);
+
+CREATE TABLE character (
+id PRIMARY KEY INTEGER,
+name TEXT,
+motto TEXT,
+species TEXT,
+author INTEGER,
+series INTEGER
 );
