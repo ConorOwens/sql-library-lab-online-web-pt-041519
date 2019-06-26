9c1380b02c7df8059cea6c695bb842b2da4b32a2
Conor Owens <conorj.owens@gmail.com>
2019-06-26 03:15:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -4,7 +4,10 @@ INSERT INTO subgenres (name) VALUES ("Horror");
 INSERT INTO subgenres (name) VALUES ("Fantasy");
 INSERT INTO authors (name) VALUES ("Darren Shan");
 INSERT INTO authors (name) VALUES ("Pratchett");
-INSERT INTO book (title, year, series_id) VALUES ("Cirque du Freak", 1994, 1);
-INSERT INTO book (title, year, series_id) VALUES ("The Vampires Assistant", 1996, 1);
-INSERT INTO book (title, year, series_id) VALUES ("Tunnels of Blood", 1998, 1);
-INSERT INTO book (title, year, series_id) VALUES ("The Vampires Assistant", 1994, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Cirque du Freak", 1994, 1);
+INSERT INTO books (title, year, series_id) VALUES ("The Vampires Assistant", 1996, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Tunnels of Blood", 1998, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Soul Music", 1994, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Thief of Time", 1994, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Guards! Guards!", 1994, 2);
+INSERT INTO characters ()
