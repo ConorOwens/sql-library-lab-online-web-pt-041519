f4db729413328bd5e45e7b31a4af5178d0bf819e
Conor Owens <conorj.owens@gmail.com>
2019-06-26 03:09:55 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,4 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Bongos", "1", "1");
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Hogwarts", "1", "1");
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Discworld", "2", "2");
 INSERT INTO subgenres (name) VALUES ("");
 INSERT INTO subgenres (name) VALUES ("Fantasy");
