34d090fe9f89f04dc39d1a5cc32be415f4caf7d5
Conor Owens <conorj.owens@gmail.com>
2019-06-26 03:06:11 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,5 @@
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Bongos", "1", "1");
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Horns", "2", "2");
-INSERT INTO subgenres ()
+INSERT INTO subgenres (name) VALUES ("Percussion");
+INSERT INTO subgenres (name) VALUES ("Wind");
+INSERT INTO authors ()
