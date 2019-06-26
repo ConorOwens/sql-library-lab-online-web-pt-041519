84245317574c3d4fd177d447c6183b0eea8a5c32
Conor Owens <conorj.owens@gmail.com>
2019-06-26 03:40:51 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "select author.name, subgenre.name from author inner join subgenre where author.series"
 end
 
 def select_series_title_with_most_human_characters
