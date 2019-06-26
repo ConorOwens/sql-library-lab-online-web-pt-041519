649f9e92752f246da978fc7dd20f3f7e10da8263
Conor Owens <conorj.owens@gmail.com>
2019-06-26 03:47:17 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "select series.title from series join characters on character.series_id = series.id "
+  "select series.title from series join books on character.series_id = series.id "
 end
 
 def select_character_names_and_number_of_books_they_are_in
